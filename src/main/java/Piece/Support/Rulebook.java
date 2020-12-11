c2249449412db887376f937e4d88b046f75c8902
Wong Chong <wcho400@aucklanduni.ac.nz>
2020-12-11 18:51:07 +1300

    cleaned up some names

--- src/main/java/Piece/Support/Rulebook.java
+++ src/main/java/Piece/Support/Rulebook.java
@@ -12,31 +12,30 @@ import java.util.Map;
 
 public class Rulebook implements IRulebook {
     private static Rulebook instance = null;
-    private boolean _EnPassant = false;
-//    private boolean _castle = false;
+    private boolean _enPassant = false;
     private Coordinate _castle = null;
 
-    public boolean legal(IPiece movingPiece, ICoordinate endPosition, ICoordinate _lastOriginalPosition, ICoordinate _lastFinalPosition, PieceType[][] _representation, Map<Side, List<Piece>> _pieceLocation){
-        switch (movingPiece.getType()){
+    public boolean legal(IPiece movingPiece, ICoordinate endPosition, ICoordinate _lastOriginalPosition, ICoordinate _lastFinalPosition, PieceType[][] _representation, Map<Side, List<Piece>> _pieceLocation) {
+        switch (movingPiece.getType()) {
             case KING:
-                return king(movingPiece,endPosition,_representation,_pieceLocation);
+                return king(movingPiece, endPosition, _representation, _pieceLocation);
             case QUEEN:
-                return queen(movingPiece,endPosition,_representation);
+                return queen(movingPiece, endPosition, _representation);
             case BISHOP:
-                return bishop(movingPiece,endPosition,_representation);
+                return bishop(movingPiece, endPosition, _representation);
             case ROOK:
-                return rook(movingPiece,endPosition, _representation);
+                return rook(movingPiece, endPosition, _representation);
             case KNIGHT:
-                return knight(movingPiece,endPosition);
+                return knight(movingPiece, endPosition);
             default:
-                return pawn(movingPiece,endPosition, _lastOriginalPosition, _lastFinalPosition, _representation, _pieceLocation);
+                return pawn(movingPiece, endPosition, _lastOriginalPosition, _lastFinalPosition, _representation, _pieceLocation);
         }
     }
 
     @Override
     public boolean isEnPassant() {
-        if (_EnPassant){
-            _EnPassant =false;
+        if (_enPassant) {
+            _enPassant = false;
             return true;
         }
         return false;
@@ -49,72 +48,72 @@ public class Rulebook implements IRulebook {
 
     @Override
     public void resetCastle() {
-        _castle=null;
+        _castle = null;
     }
 
-    public boolean king(IPiece movingPiece, ICoordinate endPosition,PieceType[][] locations, Map<Side,List<Piece>> pieces){
-        int smallRow=getRow(movingPiece);
-        int smallColumn=getColumn(movingPiece);
-        int bigRow=endPosition.getRow();
-        int bigColumn= endPosition.getColumn();
+    public boolean king(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations, Map<Side, List<Piece>> pieces) {
+        int originRow = movingPiece.getRow();
+        int originColumn = movingPiece.getColumn();
 
-        int rowDiff = Math.abs(smallRow-bigRow);
-        int colDiff = Math.abs(smallColumn-bigColumn);
+        int bigRow = endPosition.getRow();
+        int bigColumn = endPosition.getColumn();
+
+        int rowDiff = Math.abs(originRow - bigRow);
+        int colDiff = Math.abs(originColumn - bigColumn);
 
         // check if castleable
-        if (!movingPiece.getMoved()){
-            if (bigRow==0 || bigRow==7){
+        if (!movingPiece.getMoved()) {
+            if (bigRow == 0 || bigRow == 7) {
                 //short castle
-                if (bigColumn==6){
+                if (bigColumn == 6) {
 
                     //check if rook at that position is moved or not
                     IPiece castleRook = null;
-                    for (IPiece piece: pieces.get(movingPiece.getSide())){
-                        if (piece.getRow()==bigRow && piece.getColumn()==7){
-                            castleRook=piece;
+                    for (IPiece piece : pieces.get(movingPiece.getSide())) {
+                        if (piece.getRow() == bigRow && piece.getColumn() == 7) {
+                            castleRook = piece;
                             break;
                         }
                     }
 
                     //check if rook is set or moved
-                    if (castleRook==null){
+                    if (castleRook == null) {
                         return false;
                     }
 
                     //todo need to double check
-                    if (castleRook.getMoved()){
+                    if (castleRook.getMoved()) {
                         return false;
                     }
 
-                    if (locations[bigRow][5]==null && locations[bigRow][6]==(null)){
-                        _castle = new Coordinate(bigRow,7);
+                    if (locations[bigRow][5] == null && locations[bigRow][6] == (null)) {
+                        _castle = new Coordinate(bigRow, 7);
 //                        _castle = true;
                         return true;
                     }
                     //long castle
-                } else if (bigColumn==2){
+                } else if (bigColumn == 2) {
                     //check if rook at that position is moved or not
                     IPiece castleRook = null;
-                    for (IPiece piece: pieces.get(movingPiece.getSide())){
-                        if (piece.getRow()==bigRow && piece.getColumn()==0){
-                            castleRook=piece;
+                    for (IPiece piece : pieces.get(movingPiece.getSide())) {
+                        if (piece.getRow() == bigRow && piece.getColumn() == 0) {
+                            castleRook = piece;
                             break;
                         }
                     }
 
                     //check if rook is set or moved
-                    if (castleRook==null){
+                    if (castleRook == null) {
                         return false;
                     }
 
-                    if (castleRook.getMoved()){
+                    if (castleRook.getMoved()) {
                         return false;
                     }
 
 
-
-                    if (locations[bigRow][1]==(null) && locations[bigRow][2]==(null) && locations[bigRow][3]==(null)){
-                        _castle = new Coordinate(bigRow,0);
+                    if (locations[bigRow][1] == (null) && locations[bigRow][2] == (null) && locations[bigRow][3] == (null)) {
+                        _castle = new Coordinate(bigRow, 0);
 
 //                        _castle=true;
                         return true;
@@ -127,7 +126,7 @@ public class Rulebook implements IRulebook {
         }
 
 
-        if (rowDiff>1 || colDiff>1 ){
+        if (rowDiff > 1 || colDiff > 1) {
             return false;
         }
         movingPiece.setMoved();
@@ -137,23 +136,24 @@ public class Rulebook implements IRulebook {
     /**
      * logic for queen,
      * to see if it is performing a valid 'rook' or 'bishop' move
+     *
      * @param movingPiece
      * @param endPosition
      * @param locations
      * @return
      */
-    public boolean queen(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations){
+    public boolean queen(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations) {
         int rowDiff = Math.abs(movingPiece.getRow() - endPosition.getRow());
         int colDiff = Math.abs(movingPiece.getColumn() - endPosition.getColumn());
 
         //check if 'bishop' or 'rook' move
         //rook
-        if (rowDiff==0 || colDiff==0){
-            return rook(movingPiece,endPosition,locations);
+        if (rowDiff == 0 || colDiff == 0) {
+            return rook(movingPiece, endPosition, locations);
 
             //bishop
-        } else if (rowDiff==colDiff){
-            return bishop(movingPiece,endPosition,locations);
+        } else if (rowDiff == colDiff) {
+            return bishop(movingPiece, endPosition, locations);
         }
         //otherwise invalid move
         return false;
@@ -163,12 +163,13 @@ public class Rulebook implements IRulebook {
     /**
      * logic for bishop movement
      * ensure movement is legal according to bishop capabilities
+     *
      * @param movingPiece
      * @param endPosition
      * @param locations
      * @return
      */
-    public boolean bishop(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations){
+    public boolean bishop(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations) {
         int originRow = movingPiece.getRow();
         int originColumn = movingPiece.getColumn();
 
@@ -180,36 +181,36 @@ public class Rulebook implements IRulebook {
         boolean colPositive = true;
 
         //check if the difference is not the same (didnt move in a diagonal. Helps a latter check
-        if (rowDiff!=colDiff){
+        if (rowDiff != colDiff) {
             return false;
         }
 
         //calculate which direction the piece is moving
-        if (colDiff>0) {
+        if (colDiff > 0) {
             colPositive = false;
         }
-        if (rowDiff>0) {
-            rowPositive=false;
+        if (rowDiff > 0) {
+            rowPositive = false;
         }
 
         for (int move = 1; move < rowDiff; move++) {
-            if (locations[originRow+(rowPositive ? move : - move)][originColumn+ (colPositive ? move : - move)] != null) {
+            if (locations[originRow + (rowPositive ? move : -move)][originColumn + (colPositive ? move : -move)] != null) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean pawn(IPiece movingPiece, ICoordinate endPosition, ICoordinate lastMoveOrigin, ICoordinate lastMoveFinal, PieceType[][] locations, Map<Side, List<Piece>> _pieceLocation){
-        int smallRow=getRow(movingPiece);
-        int smallColumn=getColumn(movingPiece);
-        int bigRow=endPosition.getRow();
-        int bigColumn= endPosition.getColumn();
+    public boolean pawn(IPiece movingPiece, ICoordinate endPosition, ICoordinate lastMoveOrigin, ICoordinate lastMoveFinal, PieceType[][] locations, Map<Side, List<Piece>> _pieceLocation) {
+        int originRow = movingPiece.getRow();
+        int originColumn = movingPiece.getColumn();
+        int bigRow = endPosition.getRow();
+        int bigColumn = endPosition.getColumn();
 
-        int rowDiff = Math.abs(smallRow-bigRow);
-        int colDiff = Math.abs(smallColumn-bigColumn);
+        int rowDiff = Math.abs(originRow - bigRow);
+        int colDiff = Math.abs(originColumn - bigColumn);
 
-        if ((movingPiece.getSide().equals(Side.WHITE) && bigRow - smallRow < 0) || (movingPiece.getSide().equals(Side.BLACK) && bigRow - smallRow > 0)) {
+        if ((movingPiece.getSide().equals(Side.WHITE) && bigRow - originRow < 0) || (movingPiece.getSide().equals(Side.BLACK) && bigRow - originRow > 0)) {
             return false;
         }
 
@@ -217,12 +218,12 @@ public class Rulebook implements IRulebook {
         if (colDiff == 0) {
             if (rowDiff == 2) {
                 //check if original location is on 2 or 7
-                if (smallRow != 1 && smallRow != 6) {
+                if (originRow != 1 && originRow != 6) {
                     return false;
                 }
 
                 //check space in between
-                if (locations[(movingPiece.getSide().equals(Side.WHITE) ? 2 : 5)][smallColumn] != null) {
+                if (locations[(movingPiece.getSide().equals(Side.WHITE) ? 2 : 5)][originRow] != null) {
                     return false;
                 }
 
@@ -243,28 +244,28 @@ public class Rulebook implements IRulebook {
             }
 
             //check if end location is a piece
-            if (locations[bigRow][bigColumn]!=null){
+            if (locations[bigRow][bigColumn] != null) {
                 return true;
             }
 
             //calculate en passant
             // check if pawn previous move
             //TODO DOUBLE CHECK
-            if (!((locations[lastMoveFinal.getRow()][lastMoveFinal.getColumn()]) ==PieceType.PAWN)) {
+            if (!((locations[lastMoveFinal.getRow()][lastMoveFinal.getColumn()]) == PieceType.PAWN)) {
                 return false;
             }
 
             //check if pawn moved two squares
-            if (Math.abs(lastMoveFinal.getRow()-lastMoveOrigin.getRow())!=2){
+            if (Math.abs(lastMoveFinal.getRow() - lastMoveOrigin.getRow()) != 2) {
                 return false;
             }
 
             //check if last piece is next to this piece
-            if (lastMoveFinal.getRow()!=smallRow|| lastMoveFinal.getColumn()!=bigColumn){
+            if (lastMoveFinal.getRow() != originRow || lastMoveFinal.getColumn() != bigColumn) {
                 return false;
             }
 
-            _EnPassant=true;
+            _enPassant = true;
         } else {
             return false;
         }
@@ -272,39 +273,48 @@ public class Rulebook implements IRulebook {
         return true;
     }
 
-    public boolean rook(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations){
-        int smallRow=getRow(movingPiece);
-        int smallColumn=getColumn(movingPiece);
-        int bigRow=endPosition.getRow();
-        int bigColumn= endPosition.getColumn();
+    /**
+     * logic for rook movement
+     * may consider doing more lambda expresses
+     * @param movingPiece
+     * @param endPosition
+     * @param locations
+     * @return
+     */
+    public boolean rook(IPiece movingPiece, ICoordinate endPosition, PieceType[][] locations) {
+        int originRow = movingPiece.getRow();
+        int originColumn = movingPiece.getColumn();
+        int finalRow = endPosition.getRow();
+        int finalColumn = endPosition.getColumn();
 
-        int rowDiff = Math.abs(smallRow-bigRow);
-        int colDiff = Math.abs(smallColumn-bigColumn);
+        int rowDiff = (originRow - finalRow);
+        int colDiff = (originColumn - finalColumn);
 
-        if (rowDiff==0){
-            if (bigColumn<smallColumn){
+
+        if (rowDiff == 0) {
+            if (colDiff>0) {
                 //swap around
-                int dummy = bigColumn;
-                bigColumn = smallColumn;
-                smallColumn = dummy;
+                int dummy = finalColumn;
+                finalColumn = originColumn;
+                originColumn = dummy;
             }
 
-            for (int column = smallColumn +1 ; column<bigColumn;column++){
-                if (locations[smallRow][column]!=null){
+            for (int column = originColumn + 1; column < finalColumn; column++) {
+                if (locations[originRow][column] != null) {
                     return false;
                 }
             }
 
-        } else if (colDiff==0){
-            if (bigRow<smallRow){
+        } else if (colDiff == 0) {
+            if (rowDiff>0) {
                 //swap around
-                int dummy = bigRow;
-                bigRow = smallRow;
-                smallRow = dummy;
+                int dummy = finalRow;
+                finalRow = originRow;
+                originRow = dummy;
             }
 
-            for (int row = smallRow +1 ; row<bigRow;row++){
-                if (locations[row][smallColumn]!=null){
+            for (int row = originRow + 1; row < finalRow; row++) {
+                if (locations[row][originColumn] != null) {
                     return false;
                 }
             }
@@ -314,18 +324,24 @@ public class Rulebook implements IRulebook {
         return true;
     }
 
-    public boolean knight(IPiece movingPiece, ICoordinate endPosition){
+    /**
+     * logic for knight movement
+     * @param movingPiece
+     * @param endPosition
+     * @return
+     */
+    public boolean knight(IPiece movingPiece, ICoordinate endPosition) {
         //need to check if a legal knight move has taken place
-        int rowDiff = Math.abs(movingPiece.getRow()-endPosition.getRow());
-        int colDiff = Math.abs(movingPiece.getColumn()-endPosition.getColumn());
+        int rowDiff = Math.abs(movingPiece.getRow() - endPosition.getRow());
+        int colDiff = Math.abs(movingPiece.getColumn() - endPosition.getColumn());
 
         //check if the difference is not the same (didnt move in a diagonal. Helps a latter check
-        if (rowDiff==colDiff){
+        if (rowDiff == colDiff) {
             return false;
         }
 
         //check if the knight is moving in a L pattern
-        if ((rowDiff != 1 && rowDiff != 2) || (colDiff != 1 && colDiff !=2 )) {
+        if ((rowDiff != 1 && rowDiff != 2) || (colDiff != 1 && colDiff != 2)) {
             return false;
         }
 
@@ -335,8 +351,10 @@ public class Rulebook implements IRulebook {
 
 
     //singleton class to be referenced in the board
+
     /**
      * Get the instance of the CourseMgr class.
+     *
      * @return the singleton instance.
      */
     public static Rulebook getInstance() {
